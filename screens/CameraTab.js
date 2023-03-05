01eab4fa5d6fd7212a692ac8b884630ff0819fc0
Surya <suryashanmugam171@gmail.com>
2023-03-05 21:08:45 +0530

    Camera integration done

--- screens/CameraTab.js
+++ screens/CameraTab.js
@@ -3,12 +3,14 @@ import {createStackNavigator} from '@react-navigation/stack';
 import CameraResultTab from './CameraResultTab';
 import PreviousConverstionTab from './PreviousConversationTab';
 import NewConverstionTab from './NewConversationTab';
+import ImageCapture from './ImageCapture';
 
 const Stack = createStackNavigator();
 
 function CameraTab() {
   return (
     <Stack.Navigator screenOptions={{header: () => null}}>
+      <Stack.Screen name="ImageCapture" component={ImageCapture}/>
       <Stack.Screen name="CameraResultTab" component={CameraResultTab} />
       <Stack.Screen
         name="PreviousConverstionTab"
